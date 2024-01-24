2264be8e89e2238c30b111c51127d970f0b47bd1
qpsqps123 <55061153+qpsqps123@users.noreply.github.com>
2024-01-24 18:04:30 +0900

    [feat] 계산 범위 초과 여부 판단 함수 추가
    - 계산 가능 범위 하한은 2.23e-308, 상한은 1.7e308.
    - 자세한 사항은 함수 JSDoc 참고.

---
+++ script/utils/validate.js
@@ -0,0 +1,16 @@
+/**
+ * 현재 계산 결과 값이 계산 가능 범위를 초과했는지 판단합니다.
+ * @param {number} value 현재 계산 결과 값을 인자로 받습니다.
+ * @returns {number | string} 현재 계산 결과 값이 계산 가능 범위를 초과하면, 문자열 Out of range를 반환합니다.
+ */
+export function validateResultValueRange(value) {
+  if (value === 0) {
+    return 0;
+  } else if (value < 2.23e-308) {
+    value = "Out of range";
+    return value;
+  } else if (value > 1.7e308) {
+    value = "Out of range";
+    return value;
+  }
+}
