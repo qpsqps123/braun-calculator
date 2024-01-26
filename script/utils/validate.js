5faf01454d6563f73359e8aa9b1fb53e3108fb9e
qpsqps123 <55061153+qpsqps123@users.noreply.github.com>
2024-01-26 14:58:18 +0900

    [fix] 계산 결과 값이 음수 일 때, 범위 초과("Out of range")라고 뜨는 문제
    - 기존에는 계산 결과 값의 범위 초과 여부 검사 조건에 음수에 대한 처리가 되어있지 않았음. 그러므로, 양수에서 0 다음 표현 가능한 가장 작은 숫자(2.23e-308), 음수에서 0 다음 표현 가능한 가장 큰 숫자(-2.23e-308), 음수에서 표현 가능한 가장 작은 숫자(-1.7e308) 범위를 조건에 추가함.

--- script/utils/validate.js
+++ script/utils/validate.js
@@ -4,9 +4,16 @@
  * @returns {number | string} 현재 계산 결과 값이 계산 가능 범위를 초과하면, 문자열 Out of range를 반환합니다.
  */
 export function validateResultValueRange(value) {
+  console.log(value < 2.23e-308);
   if (value === 0) {
     return 0;
-  } else if (value < 2.23e-308) {
+  } else if (value > 0 && value < 2.23e-308) {
+    value = "Out of range";
+    return value;
+  } else if (value < 0 && value > -2.23e-308) {
+    value = "Out of range";
+    return value;
+  } else if (value < -1.7e308) {
     value = "Out of range";
     return value;
   } else if (value > 1.7e308) {
