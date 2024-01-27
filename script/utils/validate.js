5c97eb32523f96a9dbf3bf2d77c53a63b17261f6
qpsqps123 <55061153+qpsqps123@users.noreply.github.com>
2024-01-27 19:55:04 +0900

    [chore] 불필요한 코드 제거
    - 불필요한 console.log 코드 제거.

--- script/utils/validate.js
+++ script/utils/validate.js
@@ -4,7 +4,6 @@
  * @returns {number | string} 현재 계산 결과 값이 계산 가능 범위를 초과하면, 문자열 Out of range를 반환합니다.
  */
 export function validateResultValueRange(value) {
-  console.log(value < 2.23e-308);
   if (value === 0) {
     return 0;
   } else if (value > 0 && value < 2.23e-308) {
