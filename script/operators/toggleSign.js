6063a3dc44874d1aaeb55570b5e51cf229e1c064
qpsqps123 <55061153+qpsqps123@users.noreply.github.com>
2024-01-27 17:01:34 +0900

    [fix] 계산 결과 값이 부호 변경 연산자 적용이 되지 않는 문제
    - 연산 후, 계산 결과 값이 첫 번째 피연산자에 할당된 상태에서 부호 변경 연산이 되지 않는 문제가 있었다. toggleSign.js의 toggleSign 함수에서 부호 변경 연산 실행조건에 flag.operandA를 줬기 때문이었다. 앞서 말한 상태는 operandA[n] 값만 할당되고 flag.operandA는 할당되어 있지 않기 때문에, flag.operandA === true 조건 대신 operandA[n] === true 조건을 줘서 해결했다(flag.operandB도 변경해주었다).

--- script/operators/toggleSign.js
+++ script/operators/toggleSign.js
@@ -1,11 +1,11 @@
 import { targetDisplayValue } from "../main.js";
-import { operandA, operandB, operator, n, flag } from "../data/globalData.js";
+import { operandA, operandB, operator, n } from "../data/globalData.js";
 
 export function toggleSign() {
-  if (!operator[n] && flag.operandA) {
+  if (!operator[n] && operandA[n]) {
     operandA[n] = operandA[n] * -1;
     targetDisplayValue.textContent = operandA[n];
-  } else if (operator[n] && flag.operandB) {
+  } else if (operator[n] && operandB[n]) {
     operandB[n] = operandB[n] * -1;
     targetDisplayValue.textContent = operandB[n];
   }
