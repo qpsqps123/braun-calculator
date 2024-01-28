5dadf4d9e8e2669f9587f4df825ccbc6a1b02ecb
qpsqps123 <55061153+qpsqps123@users.noreply.github.com>
2024-01-28 17:38:32 +0900

    [chore] 유틸 함수 사용 범위 확장에 따른 JSDoc 설명 수정

--- script/utils/format.js
+++ script/utils/format.js
@@ -2,7 +2,7 @@
  * 배정밀도 부동소수점 방식의 상대 반올림 오차 문제를 해결합니다.
  * 인자 값이 일반 표기법이면 유효숫자 16번째 자리에서 반올림합니다.
  * 인자 값이 지수 표기법이면 가수부 소수점 15번째 자리에서 반올림합니다.
- * @param {number} value 현재 계산 결과 값을 인자로 받습니다.
+ * @param {number} value 반올림시킬 값을 인자로 받습니다.
  * @returns {number} 반올림된 값을 반환합니다.
  */
 export function setResultValueFormat(value) {
@@ -20,9 +20,9 @@ export function setResultValueFormat(value) {
 }
 
 /**
- * 현재 계산 결과 값을 받아 계산기 디스플레이에 표시하기 위한 형식으로 변환합니다.
- * @param {number} value 현재 계산 결과 값을 인자로 받습니다.
- * @returns {number | string} 현재 계산 결과 값의 유효숫자가 10개를 초과하면, 지수 표기법으로 변경하며 가수의 소수점 아래 5자리에서 반올림하여 반환합니다. 초과하지 않으면 값 그대로 반환합니다.
+ * 값을 받아 계산기 디스플레이에 표시하기 위한 형식으로 변환합니다.
+ * @param {number} value 계산기 디스플레이에 표시할 값을 인자로 받습니다.
+ * @returns {number | string} 값의 유효숫자가 10개를 초과하면, 지수 표기법으로 변경하며 가수의 소수점 아래 5자리에서 반올림하여 반환합니다. 초과하지 않으면 값 그대로 반환합니다.
  */
 export function setDisplayFormat(value) {
   if (value === "Out of range") {
--- script/utils/validate.js
+++ script/utils/validate.js
@@ -1,7 +1,7 @@
 /**
- * 현재 계산 결과 값이 계산 가능 범위를 초과했는지 판단합니다.
- * @param {number} value 현재 계산 결과 값을 인자로 받습니다.
- * @returns {number | string} 현재 계산 결과 값이 계산 가능 범위를 초과하면, 문자열 Out of range를 반환합니다.
+ * 값이 계산 가능 범위를 초과했는지 판단합니다.
+ * @param {number} value 범위 유효성을 검사할 값을 인자로 받습니다.
+ * @returns {number | string} 값이 계산 가능 범위를 초과하면, 문자열 Out of range를 반환합니다.
  */
 export function validateResultValueRange(value) {
   if (value === 0) {
